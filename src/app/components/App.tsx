e6898bc34813b04f0cf23d389a3d3976e55397a2
Nir Hadassi <nir@soluto.com>
2019-08-03 17:24:00 +0300

    Figma Plugin React Template

---
+++ src/app/components/App.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import '../styles/ui.css';
+
+declare function require(path: string): any;
+
+const App = ({}) => {
+    const textbox = React.useRef<HTMLInputElement>(undefined);
+
+    const countRef = React.useCallback((element: HTMLInputElement) => {
+        if (element) element.value = '5';
+        textbox.current = element;
+    }, []);
+
+    const onCreate = React.useCallback(() => {
+        const count = parseInt(textbox.current.value, 10);
+        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
+    }, []);
+
+    const onCancel = React.useCallback(() => {
+        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
+    }, []);
+
+    return (
+        <div>
+            <img src={require('../assets/logo.svg')} />
+            <h2>Rectangle Creator</h2>
+            <p>
+                Count: <input ref={countRef} />
+            </p>
+            <button id="create" onClick={onCreate}>
+                Create
+            </button>
+            <button onClick={onCancel}>Cancel</button>
+        </div>
+    );
+};
+
+export default App;
