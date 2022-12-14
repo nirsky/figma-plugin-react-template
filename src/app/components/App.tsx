84f1862064ce5127a53c22e834c4255cbc503ed1
Nir Hadassi <nir@island.io>
2022-12-14 11:10:04 +0200

    chore: add CI and update prettier config

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -2,45 +2,45 @@ import React from 'react';
 import '../styles/ui.css';
 
 function App() {
-    const textbox = React.useRef<HTMLInputElement>(undefined);
+  const textbox = React.useRef<HTMLInputElement>(undefined);
 
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
+  const countRef = React.useCallback((element: HTMLInputElement) => {
+    if (element) element.value = '5';
+    textbox.current = element;
+  }, []);
 
-    const onCreate = () => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
+  const onCreate = () => {
+    const count = parseInt(textbox.current.value, 10);
+    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*');
+  };
+
+  const onCancel = () => {
+    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
+  };
+
+  React.useEffect(() => {
+    // This is how we read messages sent from the plugin controller
+    window.onmessage = (event) => {
+      const { type, message } = event.data.pluginMessage;
+      if (type === 'create-rectangles') {
+        console.log(`Figma Says: ${message}`);
+      }
     };
+  }, []);
 
-    const onCancel = () => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    };
-
-    React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
-        window.onmessage = (event) => {
-            const {type, message} = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
-            }
-        };
-    }, []);
-
-    return (
-        <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
-            <button id="create" onClick={onCreate}>
-                Create
-            </button>
-            <button onClick={onCancel}>Cancel</button>
-        </div>
-    );
+  return (
+    <div>
+      <img src={require('../assets/logo.svg')} />
+      <h2>Rectangle Creator</h2>
+      <p>
+        Count: <input ref={countRef} />
+      </p>
+      <button id="create" onClick={onCreate}>
+        Create
+      </button>
+      <button onClick={onCancel}>Cancel</button>
+    </div>
+  );
 }
 
 export default App;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
 document.addEventListener('DOMContentLoaded', function () {
-    const container = document.getElementById('react-page');
-    const root = createRoot(container);
-    root.render(<App />);
+  const container = document.getElementById('react-page');
+  const root = createRoot(container);
+  root.render(<App />);
 });
